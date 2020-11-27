284711bea295bb95f3c7344111025597cb136f68
Amal Mohan K <amalmohan542@gmail.com>
2020-11-27 23:58:30 +0530

    Loop unrolling Program added

---
+++ 12_LOOP_UNROLLING/LOOP_UNROLLING.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+void main()
+{
+    unsigned int n;
+    int x, d;
+    char ch;
+    printf("\nEnter N\n");
+    scanf("%u", &n);
+    printf("\n1. Loop Roll\n2. Loop UnRoll\n");
+    do
+    {
+        printf("\nEnter ur choice\n");
+        scanf(" %c", &ch);
+        switch (ch)
+        {
+        case '1':
+            x = countbit1(n);
+            printf("\nLoop Roll: Count of 1's : %d", x);
+            break;
+        case '2':
+            x = countbit2(n);
+            printf("\nLoop UnRoll: Count of 1's : %d", x);
+            break;
+        default:
+            printf("\n Wrong Choice\n");
+        }
+        printf("\ndo you want to continue\n");
+        scanf("%d", &d);
+    } while (d == 1);
+}
+int countbit1(unsigned int n)
+{
+    int bits = 0, i = 0;
+    while (n != 0)
+    {
+        if (n & 1)
+            bits++;
+        n >>= 1;
+        i++;
+    }
+    printf("\n no of iterations %d", i);
+    return bits;
+}
+int countbit2(unsigned int n)
+{
+    int bits = 0, i = 0;
+    while (n != 0)
+    {
+        if (n & 1)
+            bits++;
+        if (n & 2)
+            bits++;
+        if (n & 4)
+            bits++;
+        if (n & 8)
+            bits++;
+        n >>= 4;
+        i++;
+    }
+    printf("\n no of iterations %d", i);
+    return bits;
+}
